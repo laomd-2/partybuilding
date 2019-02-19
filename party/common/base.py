17f25c61f568941dadfcf4c94e433352691f787d
laomd-2 <laomd@mail2.sysu.edu.cn>
2019-02-19 19:21:17 +0800

    django2.1使用xadmin

--- party/common/base.py
+++ party/common/base.py
@@ -7,7 +7,38 @@ class AdminObject(object):
     def bind_member(self):
         return get_bind_member(self.request.user)
 
+    def has_view_permission(self, obj=None):
+        view_codename = get_permission_codename('view', self.opts)
+        change_codename = get_permission_codename('change', self.opts)
+
+        return ('view' not in self.remove_permissions) and \
+               (self.user.has_perm('%s.%s' % (self.app_label, view_codename)) or
+                self.user.has_perm('%s.%s' % (self.app_label, change_codename))
+                ) and self.view_(obj)
+
+    def has_add_permission(self):
+        codename = get_permission_codename('add', self.opts)
+        return ('add' not in self.remove_permissions) and \
+               self.user.has_perm('%s.%s' % (self.app_label, codename)) and self.add_()
+
+    def has_change_permission(self, obj=None):
+        codename = get_permission_codename('change', self.opts)
+        return ('change' not in self.remove_permissions) and \
+               self.user.has_perm('%s.%s' % (self.app_label, codename)) and self.change_(obj)
+
     def has_delete_permission(self, request=None, obj=None):
         codename = get_permission_codename('delete', self.opts)
-        return ('delete' not in self.remove_permissions) and self.user.has_perm('%s.%s' % (self.app_label, codename))
+        return ('delete' not in self.remove_permissions) and \
+               self.user.has_perm('%s.%s' % (self.app_label, codename)) and self.delete_(request, obj)
 
+    def view_(self, obj):
+        return True
+
+    def add_(self):
+        return True
+
+    def change_(self, obj):
+        return True
+
+    def delete_(self, reques, obj):
+        return True
--- party/xadmin/views/base.py
+++ party/xadmin/views/base.py
@@ -587,18 +587,6 @@ class ModelAdminView(CommAdminView):
         """
         return self.model._default_manager.get_queryset()
 
-    def view_(self, obj):
-        return True
-
-    def add_(self):
-        return True
-
-    def change_(self, obj):
-        return True
-
-    def delete_(self, reques, obj):
-        return True
-
     def has_view_permission(self, obj=None):
         view_codename = get_permission_codename('view', self.opts)
         change_codename = get_permission_codename('change', self.opts)
@@ -606,19 +594,19 @@ class ModelAdminView(CommAdminView):
         return ('view' not in self.remove_permissions) and \
                (self.user.has_perm('%s.%s' % (self.app_label, view_codename)) or
                 self.user.has_perm('%s.%s' % (self.app_label, change_codename))
-                ) and self.view_(obj)
+                )
 
     def has_add_permission(self):
         codename = get_permission_codename('add', self.opts)
         return ('add' not in self.remove_permissions) and \
-               self.user.has_perm('%s.%s' % (self.app_label, codename)) and self.add_()
+               self.user.has_perm('%s.%s' % (self.app_label, codename))
 
     def has_change_permission(self, obj=None):
         codename = get_permission_codename('change', self.opts)
         return ('change' not in self.remove_permissions) and \
-               self.user.has_perm('%s.%s' % (self.app_label, codename)) and self.change_(obj)
+               self.user.has_perm('%s.%s' % (self.app_label, codename))
 
     def has_delete_permission(self, request=None, obj=None):
         codename = get_permission_codename('delete', self.opts)
         return ('delete' not in self.remove_permissions) and \
-               self.user.has_perm('%s.%s' % (self.app_label, codename)) and self.delete_(request, obj)
+               self.user.has_perm('%s.%s' % (self.app_label, codename))
