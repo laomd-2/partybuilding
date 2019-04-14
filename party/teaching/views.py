5dc371d2cdc5e1cfd48f66fe05127fcad37c8215
laomd-2 <laomd@mail2.sysu.edu.cn>
2019-04-14 12:43:54 +0800

    使用RPC解耦合

--- party/teaching/views.py
+++ party/teaching/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from robot.daka.consumer import consume
+from robot.daka.producer import producer
+import threading
 # Create your views here.
+
+
+threading.Thread(target=producer).start()
+threading.Thread(target=consume).start()
